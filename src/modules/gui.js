fd1b49cbc7c9f83194074dd86565662c2828ee23
Derek <19647340+derekdinan@users.noreply.github.com>
2022-04-08 11:39:59 +1000

    Add hotbar GUI

--- src/modules/gui.js
+++ src/modules/gui.js
@@ -5,7 +5,57 @@ window.manic.GUI = (function(manic) {
         chatlog = null,
         chatbox = null;
 
+    function drawHotbar() {
+        let table = document.createElement('table');
+        table.id = 'hotbar';
+        let thead = document.createElement('thead');
+        let tbody = document.createElement('tbody');
+
+        table.appendChild(thead);
+        table.appendChild(tbody);
+
+        document.body.appendChild(table);
+
+        addHotbarData(thead);
+    }
+
+    function addHotbarData(thead) {
+        let row = document.createElement('tr');
+        let slot1 = document.createElement('th');
+        slot1.innerHTML = "1";
+        let slot2 = document.createElement('th');
+        slot2.innerHTML = "2";
+        let slot3 = document.createElement('th');
+        slot3.innerHTML = "3";
+        let slot4 = document.createElement('th');
+        slot4.innerHTML = "4";
+        let slot5 = document.createElement('th');
+        slot5.innerHTML = "5";
+        let slot6 = document.createElement('th');
+        slot6.innerHTML = "6";
+        let slot7 = document.createElement('th');
+        slot7.innerHTML = "7";
+        let slot8 = document.createElement('th');
+        slot8.innerHTML = "8";
+        let slot9 = document.createElement('th');
+        slot9.innerHTML = "9";
+
+        row.appendChild(slot1);
+        row.appendChild(slot2);
+        row.appendChild(slot3);
+        row.appendChild(slot4);
+        row.appendChild(slot5);
+        row.appendChild(slot6);
+        row.appendChild(slot7);
+        row.appendChild(slot8);
+        row.appendChild(slot9);
+
+        thead.appendChild(row);
+    }
+
     function init(sendMessage) {
+        drawHotbar();
+        // Chat logs
         chatlog = document.createElement('div');
         chatlog.id = 'chatlog';
         document.body.appendChild(chatlog);
